5516d14df0605549788796a9376e9ab795030574
acheinu1 <acheinue@gmail.com>
2020-05-17 22:44:01 +0100

    my advanced js calculator

---
+++ assets/js/calculator.js
@@ -0,0 +1,30 @@
+var box = document.getElementById("screen");
+
+// add values to the screen 
+function toScreen (x){
+    box.value +=x;
+    if(x ==="c"){
+        box.value =null;
+    }
+}
+
+// the equal button
+// var equalto = document.getElementById("equal");
+
+function equal(){
+    x = box.value
+    x = eval(x);
+    box.value = x;
+}
+
+
+function power(){
+    x = box.value
+    x = eval(x * x)
+    box.value=x
+}
+
+function backspace(){
+    var display = box.value
+    box.value = display.substring(0, display.length-1)
+}
