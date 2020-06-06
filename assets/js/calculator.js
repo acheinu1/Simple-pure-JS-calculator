b62bd12d4580e076b9f84812b8693fa54a251d5c
acheinu1 <acheinue@gmail.com>
2020-06-06 05:38:16 +0100

    tried out array slice method

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,30 +1,36 @@
-var box = document.getElementById("screen");
+var box = document.getElementById('screen');
 
-// add values to the screen 
-function toScreen (x){
-    box.value +=x;
-    if(x ==="c"){
-        box.value =null;
-    }
+// add values to the screen
+function toScreen(x) {
+  box.value += x;
+  if (x === 'c') {
+    box.value = null;
+  }
 }
 
 // the equal button
 // var equalto = document.getElementById("equal");
 
-function equal(){
-    x = box.value
-    x = eval(x);
-    box.value = x;
+function equal() {
+  x = box.value;
+  x = eval(x);
+  box.value = x;
 }
 
-
-function power(){
-    x = box.value
-    x = eval(x * x)
-    box.value=x
+function power() {
+  x = box.value;
+  x = eval(x * x);
+  box.value = x;
 }
 
-function backspace(){
-    var display = box.value
-    box.value = display.substring(0, display.length-1)
+// function backspace() {
+//   var display = box.value;
+//   box.value = display.slice(0, display.length - 1);
+// }
+
+// substring or slice can work for backspace btn
+
+function backspace() {
+  var display = box.value;
+  box.value = display.substring(0, display.length - 1);
 }
